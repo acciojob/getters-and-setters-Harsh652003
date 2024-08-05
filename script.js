184c9ed63b55a0ea121698db2baaeda7f48554c3
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-08-06 00:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ class Student extends Person {
 
 	study()
 	{
-		console.log(this.name, " is studying")
+		console.log(this.name,"is studying")
 	}
 }
 
 class Teacher extends Person {
 	teach()
 	{
-		console.log(this.name, " is teaching")
+		console.log(this.name,"is teaching")
 	}
 }
 const person= new Person("john", 25)
